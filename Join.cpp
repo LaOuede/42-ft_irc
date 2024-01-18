87876782f540515a0bc173677fa45c537573a9f4
Gwen <gwenola.leroux@gmail.com>
2024-01-18 11:09:08 -0500

    JOIN channels names and key map created

--- Join.cpp
+++ Join.cpp
@@ -5,10 +5,12 @@
 /* ************************************************************************** */
 /* Defines                                                                    */
 /* ************************************************************************** */
-#define ERR_UNKNOWNERROR(token) "400 JOIN :Missing # at the begining of channel name '" + token + "'\r\n"
+#define ERR_UNKNOWNERROR(name) "400 JOIN :Missing # at the begining of channel name '" + name + "'\r\n"
 #define ERR_NEEDMOREPARAMS "461 JOIN :Not enough parameters\r\n"
 #define ERR_TOOMANYPARAMS "400 JOIN :Too many parameters\r\n"
-#define ERR_BADPARAMS "400 JOIN :Number of keys is superior to number of channels\r\n"
+#define ERR_TOOMANYKEYS "400 JOIN :Number of keys is superior to number of channels\r\n"
+#define ERR_TOOMANYCHANNELS "400 JOIN :Trying to connect to too many channels at the same time\r\n"
+
 
 /* ************************************************************************** */
 /* Constructors and Destructors                                               */
@@ -27,15 +29,24 @@ Join::~Join() {}
 /* ************************************************************************** */
 /* Functions                                                                  */
 /* ************************************************************************** */
+// MAIN FUNCTION
 string Join::executeCommand(Server *server) {
 	cout << "Server dealing with : " << this->getCommandName() << " function" << endl;
 
+	// 1. PARSING
 	this->_error_msg = parseCommand(server);
 	if (this->_error_msg.compare("") != 0)
 		return this->_error_msg;
+	// 2. Get a map from the request in order to process
+	createChannelsMap();
+	// 3. Process connections
+	processChannelsConnections();
+	cleanup();
 	return (":user!d@localhost JOIN general\r\n");
 }
 
+
+//1. COMMAND PARSING
 string Join::parseCommand(Server *server) {
 	list<string> command = server->get_command_handler().get_command_tokens();
 	
@@ -47,9 +58,6 @@ string Join::parseCommand(Server *server) {
 	if (!this->_error_msg.empty()) {
 		return this->_error_msg;
 	}
-/* 	if (token[0] != '#') {
-		return ERR_UNKNOWNERROR(token);
-	} */
 	return "";
 }
 
@@ -86,14 +94,74 @@ void Join::splitParameters(string to_split, list<string> &to_fill) {
 
 string Join::parseAttributes(const list<string> &command) {
 	splitParameters(command.front(), this->_channels_names);
-	splitParameters(command.back(), this->_channels_keys);
-
+	if (command.size() == 2) {
+		splitParameters(command.back(), this->_channels_keys);
+	}
+	if (this->_channels_names.size() > 3) {
+		return ERR_TOOMANYCHANNELS;
+	}
 	if (this->_channels_keys.size() > this->_channels_names.size()) {
-		return ERR_BADPARAMS;
+		return ERR_TOOMANYKEYS;
 	}
 	return "";
 }
 
+//2. CREATING A MAP<CHANNEL_NAME, CHANNEL_KEY>
+void Join::createChannelsMap() {
+	string name;
+	string key;
+	size_t map_size = this->_channels_names.size();
+
+	for (size_t i = 0; i < map_size; i++) {
+		if (this->_channels_keys.empty()) {
+			key = "";
+		} else {
+			key = this->_channels_keys.front();
+			this->_channels_keys.pop_front();
+		}
+		this->_channels_map[this->_channels_names.front()] = key;
+		this->_channels_names.pop_front();
+	}
+
+	// DEBUG Print map
+	cout << "--- Elements in map ---" << endl;
+	map<string, string>::const_iterator it;
+	int index = -1;
+	it = this->_channels_map.begin();
+	for (; it != this->_channels_map.end(); ++it) {
+		std::cout << "index " << ++index << " : " << it->first << " - " << it->second << std::endl;
+	}
+	cout << "\n" << endl;
+}
+
+//3. PROCESS CONNECTIONS
+string Join::processChannelsConnections() {
+	map<string, string>::const_iterator it;
+
+	it = this->_channels_map.begin();
+	for (; it != this->_channels_map.end(); ++it) {
+		this->_error_msg = parseChannelName(it->first);
+	}
+	return "";
+}
+
+string Join::parseChannelName(string name) {
+	if (name[0] != '#') {
+		return ERR_UNKNOWNERROR(name);
+	}
+	return "";
+}
+
+
+void Join::cleanup() {
+	this->_channels_names.clear();
+	this->_channels_keys.clear();
+}
+
+
+/* 	if (token[0] != '#') {
+		return ERR_UNKNOWNERROR(token);
+	} */
 
 /* ************************************************************************** */
 /* Exceptions                                                                 */
--- Join.hpp
+++ Join.hpp
@@ -6,11 +6,13 @@
 # include "ACommand.hpp"
 # include <iostream>
 # include <list>
+# include <map>
 # include <string>
 
 using std::cout;
 using std::endl;
 using std::list;
+using std::map;
 using std::string;
 
 class Server;
@@ -26,14 +28,19 @@ class Join : public ACommand {
 		string parseCommand(Server *server);
 		string parseParameters(const list<string> &command);
 		void splitParameters(string to_split, list<string> &to_fill);
-		string parseAttributes(const list<string> &command); 
+		string parseAttributes(const list<string> &command);
+		void createChannelsMap();
+		string processChannelsConnections();
+		string parseChannelName(string name);
+		void cleanup();
 
 	private:
 		// Attributes
-		string			_name;
-		string			_error_msg;
-		list<string>	_channels_names;
-		list<string>	_channels_keys;
+		string				_name;
+		string				_error_msg;
+		list<string>		_channels_names;
+		list<string>		_channels_keys;
+		map<string, string>	_channels_map;
 
 };
 
