a10da511e674a5bc371a44f87c7e3ee011074e9f
Gwen <gwenola.leroux@gmail.com>
2024-01-23 16:39:04 -0500

    feature: PART reason added

--- Part.cpp
+++ Part.cpp
@@ -1,14 +1,18 @@
 # include "Part.hpp"
 # include "Server.hpp"
+# include "Channel.hpp"
 
 /* ************************************************************************** */
 /* Defines                                                                    */
 /* ************************************************************************** */
 #define ERR_NEEDMOREPARAMS(function) "461 " + function + " :Not enough parameters\r\n"
 #define ERR_NOSUCHCHANNEL(function, channel) "463 " + function + "'" + channel + "' :No such channel\r\n"
+#define ERR_REASONBADFORMAT(function) "400 " + function + " :Reason bad format (eg. ':Reason')\r\n"
+#define ERR_REASONTOOLONG(function) "400 " + function + " :Reason is too long (max. 10 characters)\r\n"
 #define ERR_TOOMANYCHANNELSDECONNECTION "400 PART :Trying to deconnect from too many channels\r\n"
 #define ERR_TOOMANYPARAMS(function) "400 " + function + " :Too many parameters\r\n"
 #define ERR_UNKNOWNERROR(function, name) "400 " + function + " :Missing # at the begining of channel name '" + name + "'\r\n"
+#define RPL_QUITCHANNEL(function, user, channel, reason) ": 400 " + function + " :" + user + " is leaving the channel '" + channel + "' for the reason " + reason + "\r\n"
 
 
 /* ************************************************************************** */
@@ -40,7 +44,7 @@ string Part::executeCommand(Server *server) {
 	processChannelDeconnections(server);
 	// 3. Clean Up
 	cleanup();
-	return ("");
+	return "";
 }
 
 //1. COMMAND PARSING
@@ -55,7 +59,7 @@ string Part::parseCommand(Server *server) {
 	if (!this->_error_msg.empty()) {
 		return this->_error_msg;
 	}
-	return ("");
+	return "";
 }
 
 string Part::parseParameters(const list<string> &command) {
@@ -65,15 +69,20 @@ string Part::parseParameters(const list<string> &command) {
 	if (command.size() > 2) {
 		return ERR_TOOMANYPARAMS(this->_name);
 	}
-	return ("");
+	return "";
 }
 
 string Part::parseAttributes(const list<string> &command) {
 	splitParameters(command.front(), this->_channel_name);
-	if (this->_channel_name.size() > CHANLIMIT) {
-		return ERR_TOOMANYCHANNELSDECONNECTION;
+	if (command.size() == 2) {
+		this->_reason = command.back();
+		if (this->_reason[0] != ':' || this->_reason.size() < 2 || this->_reason.find_first_not_of(CHARACTERS_ALLOWED, 1) != string::npos) {
+			return ERR_REASONBADFORMAT(this->_name);
+		} else if (this->_reason.size() > 10) {
+			return ERR_REASONTOOLONG(this->_name);
+		}
 	}
-	return ("");
+	return "";
 }
 
 void Part::splitParameters(string to_split, list<string> &to_fill) {
@@ -99,13 +108,23 @@ string Part::processChannelDeconnections(Server *server) {
 		if (mapIt != server->getChannelList().end()) {
 			Channel *channel = mapIt->second;
 			channel->removeUserFromChannel(server, user_fd);
+			if (this->_reason.empty()) {
+				this->_reason = "[No specific reason mentionned]";
+			}
+			broadcastUserQuitMessage(channel, server->getUserDB()[user_fd]._nickname, this->_reason);
 			continue;
 		} else {
 			error_msg = ERR_NOSUCHCHANNEL(this->_name, *it);
-			server->sendToClient(&error_msg);;
+			server->sendToClient(error_msg);;
 		}
 	}
-	return ("");
+	return "";
+}
+
+void Part::broadcastUserQuitMessage(Channel *channel, const string &user, const string &reason) {
+	const string &channel_name = channel->getChannelName();
+	string msg = RPL_QUITCHANNEL(this->_name, user, channel_name, reason);
+	channel->broadcastToAll(msg);
 }
 
 // 3. CLEAN UP
