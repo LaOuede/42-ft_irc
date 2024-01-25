47dd7bbc31ad19d5794d0f1efbc4587f8c3d72c8
TwindZ <emmanuel.lamothe@gmail.com>
2024-01-25 10:45:39 -0500

    done

--- Privmsg.cpp
+++ Privmsg.cpp
@@ -7,7 +7,7 @@
 /* ************************************************************************** */
 
 #define PRIVMSG(nick, target, msg) ":" + nick + " PRIVMSG " + target + " " + msg + "\r\n"
-#define ERR_CANNOTSENDTOCHAN(channel) "404 " + /*nickname + " " +*/ channel + " :Cannot send to channel\r\n"
+#define ERR_CANNOTSENDTOCHAN(nickname, channel) "404 " + nickname + " " + channel + " :Cannot send to channel\r\n"
 #define ERR_NOSUCHNICK(target) "401 " + target + " :No such nick/channel\r\n"
 #define ERR_NOTEXTTOSEND(target) "412 " + target + " :No text to send\r\n"
 #define ERR_NEEDMOREPARAMS "461 PRIVMSG :Not enough parameters\r\n"
@@ -15,7 +15,8 @@
 /* ************************************************************************** */
 /* Constructors and Destructors                                               */
 /* ************************************************************************** */
-Privmsg::Privmsg() : ACommand("PRIVMSG") {}
+Privmsg::Privmsg() : ACommand("PRIVMSG"){}
+
 
 Privmsg::~Privmsg() {}
 
@@ -23,7 +24,17 @@ Privmsg::~Privmsg() {}
 /* Functions                                                                  */
 /* ************************************************************************** */
 string Privmsg::executeCommand(Server *server) {
-	
+	parseParameter(server);
+	_nick = server->getUserDB()[server->getFds()[server->getClientIndex()].fd]._nickname;
+	_response = PRIVMSG(_nick, _target, _msg);
+	if(_target[0] == '#' || _target[0] == '&')
+		return sendToChannel(server);
+	else
+		return sendToUser(server);
+	return "";
+}
+
+string Privmsg::parseParameter(Server *server){
 	list<string>::const_iterator it = server->getCommandHandler().getCommandTokens().begin();
 	_target = *it++;
 	if(_target.empty())
@@ -35,46 +46,26 @@ string Privmsg::executeCommand(Server *server) {
 		_msg.append(" ");
 		_msg.append(*it++);
 	}
-	string nick = server->getUserDB()[server->getFds()[server->getClientIndex()].fd]._nickname;
-	string response = PRIVMSG(nick, _target, _msg);
-	if(_target[0] == '#' || _target[0] == '&')
-		return sendToChannel(server, response);
-	else
-		return sendToUser(server, response);
 	return "";
 }
 
-// string Privmsg::parseParameter(Server *server){
-// 	list<string>::const_iterator it = server->getCommandHandler().getCommandTokens().begin();
-// 	_target = *it++;
-// 	if(_target.empty())
-// 		return ERR_NEEDMOREPARAMS;
-// 	_msg = *it++;
-// 	if(_msg.empty())
-// 		return ERR_NOTEXTTOSEND(_target);
-// 	while(it != server->getCommandHandler().getCommandTokens().end()){
-// 		_msg.append(" ");
-// 		_msg.append(*it++);
-// 	}
-// }
-
-string Privmsg::sendToChannel(Server *server, string const& response){
+string Privmsg::sendToChannel(Server *server){
 	if(!server->isChannelInServer(_target))
 		return ERR_NOSUCHNICK(_target);
 	Channel *channel = server->getChannel(_target);
 	if(!channel->isUserInChannel(server->getFds()[server->getClientIndex()].fd))
-		return ERR_CANNOTSENDTOCHAN(_target);
+		return ERR_CANNOTSENDTOCHAN(_nick, _target);
 	map<int, int>::iterator it= channel->getUserList().begin();
 	for(; it != channel->getUserList().end(); it++)
 		if(it->first != server->getFds()[server->getClientIndex()].fd)
-			send(it->first, response.c_str(), response.size(), 0);
+			send(it->first, _response.c_str(), _response.size(), 0); //TODO gerer -1
 	return "";
 }
 
-string Privmsg::sendToUser(Server *server, string const& response){
+string Privmsg::sendToUser(Server *server){
 	if(!server->isNickInServer(_target))
 		return ERR_NOSUCHNICK(_target);
-	send(findTargetFd(server), response.c_str(), response.size(), 0);
+	send(findTargetFd(server), _response.c_str(), _response.size(), 0); //TODO gerer -1
 	return "";
 }
 
--- Privmsg.hpp
+++ Privmsg.hpp
@@ -24,18 +24,17 @@ class Privmsg : public ACommand {
 		// Methods
 		string	executeCommand(Server *server);
 		string 	parseParameter(Server *server);
-		void	rebuildMsg();
-		string	sendToChannel(Server *server, string const& response);
-		string 	sendToUser(Server *server, string const& response);
+		string	sendToChannel(Server *server);
+		string 	sendToUser(Server *server);
 		int		findTargetFd(Server *server);
 
 	private:
 		// Attributes
 		string _name;
 		string _response;
-		string _nick;
 		string _target;
 		string _msg;
+		string _nick;
 
 
 };
